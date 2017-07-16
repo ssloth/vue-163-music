76aae50b0f1110713a2ed80809b7f98fa5c991be
1035040175@qq.com <1035041075@qq.com>
2017-07-16 16:49:19 +0800

    '初始化项目，完成了少量页面'

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <headers></headers>
+    <contents></contents>
+    <footers></footers>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Headers from 'components/headers.vue';
+  import Contents from 'components/contents.vue';
+  import Footers from 'components/footers.vue';
+
+  export default {
+    components: {
+      Headers, Contents, Footers
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
